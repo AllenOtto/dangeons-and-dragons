d8e5d73a9ac9d646b9d2a83d0c5f7da60589a820
AllenOtto <kephamoturi@gmail.com>
2023-04-27 11:13:25 +0300

    fetching and displaying items on the DOM from spells

--- js/main.js
+++ js/main.js
@@ -4,10 +4,13 @@ async function getFetch() {
     try {
         const userInput = document.querySelector('input').value
         if(userInput) {
-            const res = await fetch(`https://www.dnd5eapi.co/api/spells`)
+            const res = await fetch(`https://www.dnd5eapi.co/api/spells/${userInput}`)
             const jsonData = await res.json();
-            document.querySelector('h2').innerHTML = jsonData.results[0].name
-            console.log(jsonData)
+            console.log(jsonData);
+            document.querySelector('h2').innerHTML = jsonData.name
+            document.querySelector('h3').innerHTML = jsonData.classes[0].name
+            document.querySelector('h4').innerHTML = jsonData.subclasses[0].name
+            document.querySelector('p').innerHTML = jsonData.desc
         } else {
             console.log("Provide some input");
         }
