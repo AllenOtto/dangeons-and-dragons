8f88bfd90bf3b7b5a746214e72ed2df7402e9dc3
AllenOtto <kephamoturi@gmail.com>
2023-04-27 11:28:56 +0300

    randomized spell class and subclass calls

--- js/main.js
+++ js/main.js
@@ -7,9 +7,12 @@ async function getFetch() {
             const res = await fetch(`https://www.dnd5eapi.co/api/spells/${userInput}`)
             const jsonData = await res.json();
             console.log(jsonData);
+            let randClass = Math.floor(Math.random() * jsonData.classes.length)
+            let randSubclass = Math.floor(Math.random() * jsonData.subclasses.length)
+            console.log(randClass + " " + randSubclass);
             document.querySelector('h2').innerHTML = jsonData.name
-            document.querySelector('h3').innerHTML = jsonData.classes[0].name
-            document.querySelector('h4').innerHTML = jsonData.subclasses[0].name
+            document.querySelector('h3').innerHTML = jsonData.classes[randClass].name
+            document.querySelector('h4').innerHTML = jsonData.subclasses[randSubclass].name
             document.querySelector('p').innerHTML = jsonData.desc
         } else {
             console.log("Provide some input");
