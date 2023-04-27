a325e4aca118234d6467403abadea20a8131fb03
AllenOtto <kephamoturi@gmail.com>
2023-04-27 23:37:50 +0300

    randomized spell class and subclass calls

--- js/main.js
+++ js/main.js
@@ -7,12 +7,26 @@ async function getFetch() {
             const res = await fetch(`https://www.dnd5eapi.co/api/spells/${userInput}`)
             const jsonData = await res.json();
             console.log(jsonData);
-            let randClass = Math.floor(Math.random() * jsonData.classes.length)
+            // let randClass = Math.floor(Math.random() * jsonData.classes.length)
             let randSubclass = Math.floor(Math.random() * jsonData.subclasses.length)
-            console.log(randClass + " " + randSubclass);
+            console.log(randSubclass);
             document.querySelector('h2').innerHTML = jsonData.name
-            document.querySelector('h3').innerHTML = jsonData.classes[randClass].name
-            document.querySelector('h4').innerHTML = jsonData.subclasses[randSubclass].name
+            const UlListOfClasses = document.createElement('ul')
+            for(let i = 0; i < jsonData.classes.length; i++) {
+                const listItemForClass = document.createElement('li')
+                UlListOfClasses.append(listItemForClass)
+                listItemForClass.innerText = jsonData.classes[i].name
+                document.querySelector('.ul-list-classes').append(UlListOfClasses)
+            }
+
+            const UlListOfSubclasses = document.createElement('ul')
+            for(let i = 0; i < jsonData.subclasses.length; i++) {
+                const listItemForSubclass = document.createElement('li')
+                UlListOfSubclasses.append(listItemForSubclass)
+                listItemForSubclass.innerText = jsonData.subclasses[i].name
+                document.querySelector('.ul-list-subclasses').append(UlListOfSubclasses)
+            }
+
             document.querySelector('p').innerHTML = jsonData.desc
         } else {
             console.log("Provide some input");
